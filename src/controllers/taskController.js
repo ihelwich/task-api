83c06baf000dc23e671c3e4b121d3188cd52c2ab
Isaiah Helwich <isaiahmhelwich@gmail.com>
2026-04-14 22:59:49 -0400

    Added the completed query parameter

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,8 +1,25 @@
 import * as taskService from '../services/taskService.js';
 
 export async function getTasks(req, res, next) {
-  const tasks = await taskService.getAllTasks();
-  res.json(tasks);
+  try {
+      const completed = req.query.completed;
+
+      if (
+        completed !== undefined &&
+        completed !== 'true' &&
+        completed !== 'false'
+      ) {
+        return res.status(400).json({
+          error: 'Query parameter completed must be true or false'
+        });
+      }
+
+    const filter = completed === undefined ? undefined : completed === 'true'
+    const tasks = await taskService.getAllTasks(filter);
+    res.json(tasks);
+  } catch (error) {
+    next(error)
+  }
 }
 
 export async function createTask(req, res, next) {
